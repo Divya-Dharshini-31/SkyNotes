4585f54334f6e60af1a217aa4fb79e30161d82a2
Divya-Dharshini-31 <sdivyadharshini315@gmail.com>
2026-07-14 22:07:36 +0530

    Dependencies installed using Spring Initialzr

---
+++ backend/skynotes/src/main/java/com/divya/skynotes/SkynotesApplication.java
@@ -0,0 +1,13 @@
+package com.divya.skynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkynotesApplication.class, args);
+	}
+
+}
---
+++ backend/skynotes/src/test/java/com/divya/skynotes/SkynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divya.skynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
