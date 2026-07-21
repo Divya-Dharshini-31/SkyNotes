3965f677269da617223c887aaf4ce3f5c5dd8385
Divya-Dharshini-31 <sdivyadharshini315@gmail.com>
2026-07-21 22:14:10 +0530

    Checking error logs

--- backend/skynotes/src/main/java/com/divya/skynotes/SkynotesApplication.java
+++ backend/skynotes/src/main/java/com/divya/skynotes/SkynotesApplication.java
@@ -1,13 +1,24 @@
 package com.divya.skynotes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SkynotesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkynotesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkynotesApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner printMongoUri(Environment environment) {
+        return args -> {
+            System.out.println("======================================");
+            System.out.println("Mongo URI: " + environment.getProperty("spring.data.mongodb.uri"));
+            System.out.println("======================================");
+        };
+    }
 }
